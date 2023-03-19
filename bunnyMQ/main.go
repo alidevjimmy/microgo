64644d2a127503bd852e37f7d626fe8f74ba5071
Ali Hamrani <ali.hamrani2001@gmail.com>
2023-03-19 22:07:26 +0330

    there is some example code of rabbitMQ basic pub/sub & more advanced pub/sub and routing and work-queue but RPC (async client-server) and topic based is missing

--- bunnyMQ/main.go
+++ bunnyMQ/main.go
@@ -2,15 +2,46 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 
-	"github.com/streadway/amqp"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-func Connect(c QeueConfig) (*amqp.Connection, error) {
+func ConnectMQ() (*amqp.Connection, error) {
 	connPattern := "amqp://%s:%s@%s:%s/%s"
-	if c.Username == "" {
-		connPattern = "amqp://%s:%s@%s:%s"
-	}
 
-	clientUrl := fmt.Sprintf(connPattern, c.Username, c.Password, c.Host, c.Port, c.Vhost)
+	clientUrl := fmt.Sprintf(connPattern, "c.Username", "c.Password", "c.Host", "c.Port", "c.Vhost")
+	conn, err := amqp.Dial(clientUrl)
+	if err != nil {
+		fmt.Println("Failed Initializing Broker Connection")
+		panic(err)
+	}
+	return conn, err
+
+}
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowHeaders:     []string{"*"},
+		AllowCredentials: true,
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
+	}))
+	e.Use(middleware.Logger())
+
+	rabbitMq, err := ConnectMQ()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer rabbitMq.Close()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	log.Fatal(e.Start(":8080"))
 }
---
+++ bunnyMQ/routing/pub/publisher.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func bodyForm(args []string) string {
+	var s string
+
+	if (len(args) < 3) || os.Args[2] == "" {
+		s = "Hello"
+	} else {
+		s = strings.Join(args[2:], " ")
+	}
+	return s
+}
+
+func severityForm(args []string) string {
+	var s string
+
+	if (len(args) < 2) || os.Args[1] == "" {
+		s = "info"
+	} else {
+		s = os.Args[1]
+	}
+	return s
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	err = ch.ExchangeDeclare(
+		"direct_logs", // name
+		"direct",      // type
+		true,          // durable
+		false,         // auto-deleted
+		false,         // internal
+		false,         // no-wait
+		nil,           // arguments
+	)
+	failOnError(err, "Failed to declare an exchange")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	body := bodyForm(os.Args)
+
+	fmt.Println(severityForm(os.Args))
+	
+	err = ch.PublishWithContext(ctx,
+		"direct_logs",         // exchange
+		severityForm(os.Args), // routing key
+		false,                 // mandatory
+		false,                 // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+
+	failOnError(err, "Failed to publish a message")
+	log.Printf(" [x] Sent %s\n", body)
+}
